94e970802bbd68fb0082a2b88362c3ae06336b37
Evgeny Stepanov <maestrojane@gmail.com>
2020-12-29 13:56:11 +0300

    change search on search by index

--- simple_search.py
+++ simple_search.py
@@ -2,15 +2,14 @@ from decorators import print_execution_time_of_function
 
 
 @print_execution_time_of_function
-def search_number_in_list(number, list_for_search):
-    for element in list_for_search:
-        if element == number:
-            print('You guessed')
-            break
+def get_element_index(element, list_for_search):
+    for index in range(len(list_for_search)):
+        if list_for_search[index] == element:
+            return f'\nIndex for element {element} is {index}'
 
 
 if __name__ == '__main__':
-    number = 100_000_000
+    number = 100_000_00
     list_for_search = list(range(1, number + 1))
 
-    search_number_in_list(number, list_for_search)
+    print(get_element_index(number, list_for_search))
