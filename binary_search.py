aa9302671765c666c8a641b2506b3d322ab4de42
Evgeny Stepanov <maestrojane@gmail.com>
2020-12-29 14:02:01 +0300

    add binary search

---
+++ binary_search.py
@@ -0,0 +1,29 @@
+from decorators import print_execution_time_of_function
+
+
+# Binary search
+@print_execution_time_of_function
+def get_element_index(element, list_for_search):
+    lower_index = 0
+    upper_index = len(list_for_search) - 1
+
+    while lower_index <= upper_index:
+        middle_index = (lower_index + upper_index) // 2
+        middle_element = list_for_search[middle_index]
+
+        if element == middle_element:
+            return f'\nIndex for element {element} is {middle_index}'
+        elif element < middle_element:
+            upper_index = middle_index - 1
+        else:
+            lower_index = middle_index + 1
+
+
+def main():
+    number = 100_000_00
+    list_for_search = list(range(1, number + 1))
+    print(get_element_index(number, list_for_search))
+
+
+if __name__ == '__main__':
+    main()
