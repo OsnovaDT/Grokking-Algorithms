a179d6ba40b8d1f4ddd12edb88ff5285de881d1c
Evgeny Stepanov <maestrojane@gmail.com>
2020-12-29 13:55:28 +0300

    add return result for function

--- decorators.py
+++ decorators.py
@@ -7,11 +7,13 @@ def print_execution_time_of_function(function):
     def wrapper(*args, **kwargs):
         call_function_time = time()
 
-        function(*args, **kwargs)
+        result = function(*args, **kwargs)
 
-        execution_time = round(time() - call_function_time, 4)
+        execution_time = round(time() - call_function_time, 7)
         print(
             f'The program was completed in {execution_time} seconds'
         )
+
+        return result
 
     return wrapper
